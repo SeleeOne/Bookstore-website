70130fbe0c0b9bb1eabbed49f08d3e1168cb7b10
Alex <92160821+SeleeOne@users.noreply.github.com>
2022-12-14 23:22:35 +0100

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,12 @@
+<?php
+echo '<header>';
+echo '<h1>BookShop</h1>';
+echo '<nav>';
+echo '<ul>';
+echo '<li><a href="index.php">Show books</a></li>';
+echo '<li><a href="add_page.php">Add books</a></li>';
+echo '<li><a href="delete_page.php">Delete books</a></li>';
+echo '</ul>';
+echo '</nav>';
+echo '</header>';
+?>
